decafd821303df79a135c65b0fc52a416d2b7aee
iashris <ashris.me@gmail.com>
2019-09-01 19:23:13 +0530

    dsf

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,7 +21,7 @@ function getTimeStamp() {
   return Math.floor(new Date().getTime() / 1000);
 }
 router.get("/", function(req, res) {
-  res.render("index.ejs");
+  res.render("index.ejs", { hhh: process.env.APP_URL || "our" });
 });
 
 router.get("/r/:id", async (req, res) => {
