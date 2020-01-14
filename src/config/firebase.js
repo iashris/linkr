4c85646c8b25a5668b32dd017edf39360e439ef5
Ashris Choudhury <ashris@qwilr.com>
2020-01-14 06:32:22 +0530

    new db

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,20 @@
 import admin from "firebase-admin";
 import serviceAccount from "./service-account";
 var firebaseConfig = {
-  apiKey: "AIzaSyBz9mmAym3FYVm1L4NHtDQLloHr_fQ3Xtg",
-  authDomain: "linkr-5278b.firebaseapp.com",
-  databaseURL: "https://linkr-5278b.firebaseio.com",
-  projectId: "linkr-5278b",
-  storageBucket: "linkr-5278b.appspot.com",
-  messagingSenderId: "364718390740",
-  appId: "1:364718390740:web:abc35da231dd452e"
+  apiKey: "AIzaSyCZgqIUN6CcwLrLOroo_mWv172W42me_Ds",
+  authDomain: "makersgully.firebaseapp.com",
+  databaseURL: "https://makersgully.firebaseio.com",
+  projectId: "makersgully",
+  storageBucket: "makersgully.appspot.com",
+  messagingSenderId: "522214830535",
+  appId: "1:522214830535:web:3d7123acc0515a5678a9e2",
+  measurementId: "G-S4838SQV0F"
 };
 
+// Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://linkr-5278b.firebaseio.com"
+  databaseURL: "https://makersgully.firebaseio.com"
 });
 
 const db = admin.firestore();
--- src/config/service-account.js
+++ src/config/service-account.js
@@ -1,14 +1,12 @@
 export default {
-  type: "service_account",
-  project_id: "linkr-5278b",
-  private_key_id: "f0b034ba7f39daa94d6fb931a81118ab8f2ff3e1",
-  private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCt73I1uE1S8A4z\nTEOgtK/KCVIfulXWvvVSqFYRlYL0psdHpw3l2wGdEckJYmDrvTHGGfYSbHXDzERh\nY9betrY0ivsW9u7QEo8ju4h9rPcZMhsZKCXNAqVmnpDYrI9UXXH8hAl1WUowUOoa\nGB95JLprEyZB2UpNbpAaJGBiWF20UEauNaFBmss11y4QFajmrvXh6//oQEIx5jAI\nRmJHQkkCbxCyUTT9MerVvGJGoI/gq7eDfHjb9WD8Z3Dx0Qmogsi7y9ezGbrmCq0K\nYOxEGHxPnP5cVRM5yF9dOAmGD8Ke+rs/pxvacn9kdvoisopP5y0IXp9O13HlJEQm\nHLNvVQfVAgMBAAECggEALfeiD2BtzwVyO0xkcwxNiO99rLTqCp7c5sNmO69vtIh/\nEgisO5rtij0xDDHbu3F8vgYOakAPv3c0eyiPmLxg2vH/+cm4w3SkL6VYJR6ZDyeW\n0ccydkPEwqIVVa4FEx6/CSU7+q9mU+6Ob+cAKlvyiGm/KpfqcxxNKRuD/XmtCgHd\ng8Ks5AvUahntKKbGlcvKnQps/+anKH65PqjYma9DuIhqVLc9gVNKfz8m9e4ppacq\n6EyABRIhs0z3pJzhc9jQz0E4PrZxzASETZcfroYrNPcUEwU/eZBJ7DbdyRgUFoG6\nZp3LNhUrl+73CfCDHay/7yvd6I7ydo0LNTdYfuCZ4QKBgQDdIzuFRWiRm8Xh12Cc\nmU1TAjyFCIGX+ibVmyg1PUIUh6lYTk7rxG8D/2u9Y+t/TCr9pQxdPO2BPZ/rUrji\nuor8g96RIO+J0KUkD2uDb5IdZ8rFhd1JK5c8M9f+GRyEr7fkmgHnwyCvML2H9vbj\nk4F5B1pDvZZv0o5YIFOakAIbcwKBgQDJWzQKJimWGY9bjiPazvFMMCK9DoQHhoei\nW5Y0kb9x+EmFWe9VgsVUDs/Io30kJv8A0tvS/uU/FqerrEVZpVUGKEneZfXrhO0o\nOZPl2YXfRekHJNg9etzG+3wKAdoY8eUOkl5ZC18ILLXLqzUh2wZQntFRsAtHwXob\nwpWxygYNlwKBgCm+I9clYfbSc8kM0eTdn1YC/w+RqnzYsInzzuQ12wprDVFo/sbM\nfrQ/e8P1X5fY9eiJRks3XBHgYxaVt+a8lc3SyqmUkQLvyqyC6KWX4voABKvCeCH+\nw36Ovr/tV4/IZ6sUUzW/hKTtS2ml+ba9ulvxQl21JPeYxFvoRj/P3UX3AoGBAKQ9\n2X/hyxAdPzsuAP8Mx/QeW2ecvIMq7922GRo+PEupe8bjKf61EWJURpZhDMV9bYlt\ndVNxCOth/lhgx5ys6GKCYGahAmwe3Bdwh7v3p2QMsVxbaGsVmZpRLcc35X9taCpv\np7e4N8cCOTOtLkS0VD1UnmakUz8viPu1x8big/d3AoGAFnxHcXmz3hJ92c79JHZp\niphI3Tq/HbeUX6+cQJhwgRSsgVy9qhfUtqWMMJJx+RnH8iKrC3K8cBmPb8riWJCh\n3gIkR4j8C7oGYx1KnzEIpNGwtewk4QH5mJrpwxhlIttRVgr4hD61HJZvS16cAPBV\nOxGSHq44Jvnici1PeuVXyXQ=\n-----END PRIVATE KEY-----\n",
-  client_email: "firebase-adminsdk-azdku@linkr-5278b.iam.gserviceaccount.com",
-  client_id: "116466264441298236796",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-azdku%40linkr-5278b.iam.gserviceaccount.com"
-};
+  "type": "service_account",
+  "project_id": "makersgully",
+  "private_key_id": "3fce033061d3eb100c3176ef77d1d6fa5f1da66e",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDg0r2XQWgaZcyx\nbY4I1lqGQGLjd+BZu5CcDq5JRrbFp+TFOt8dzpCMTZzg1ucM7zphuKESA2HujGFE\noEaZfF/DQ+RZuFymR8F+6uYJjU+jHfOcuJgP21v9kCINwovfp7r23e1UEhozbpBt\nuCKJn61+eSZK1pPxztnaTxNUECLPuoGzcs+NCEv/F4VyHEajjzGPP+GTPvKJP2z7\nwyFM1b0VDJzVgOtJEuUs5PZtqWxGRf1Qcd0FYC0yfpO6mndRU1JXiHr9egDlHXQU\nA5Wg336Uu3rDWV6Ldjg0L1Z/kGq8/Ym936Z4C20JEKmTnv0a8AccE0ogfqoTRelN\nc3sLmv+1AgMBAAECggEAKi1/IgxEQoeMolDCT8gfDYawxqnrrgnxNEXQzrB0g5pe\n4d2MlCP3nErHPqsyh4ut8T1ZAm8eBUnSybQj6pGZyaFICsj6oLtfgrcA1xPqt9hb\nprA+3uKxXF2Y9enkod3uxneWGaqXvcRlViAwYo9RViPWWj3tXXUIC9u/7YplbYDe\n5j4vAUMpoFgzx2Iww4Fs5O6d4QS8cghZO7pZsJ6FgXd+wL2zrWnRJAnrzQ1eFbbS\nGYFBe5cyrFLFs0orVE4qrfmVcFDJnN4ySTfwtu4UvGTRqrmCEAmd91Q9xHP5AoI6\nZwI8bTPJ7/TNmdLv6W0DIAzRXdnWLwZidPMlb46mlQKBgQDwKelICzRMRLL9A2Xj\nr3MlN0T5O+mQSRC5TyA1AMPYudfZm2zRtHTqU53E3DJOYGvqJ+nAczdZTBq8lqzP\nSF1kaDxAG/56LohjZNYH36SSmiPOTdMvMxqbC1pjLPGQf4cWJp0S2qsJHEC7UllJ\ndrjXldmTsZxfXahJID8CEa+V5wKBgQDvpd+e9rIP7/2hcptQyGFqwviUWtxBcN4I\nr9Stern12LfJnTiMszbhmGWopZNJDx86ER72oy1h1D7HFfBkVaLEzzTWwJ8ckpCr\nK8TYDGu2pSvWhygDi9XQpvFz/CeD1364MVNe2BFZX7XkageOHRX2w4EUeEUsGtEr\nbHKzLa4SAwKBgQChPYJBvEQAcJ86/20I0GKeOd009+R32pY/07/r1fenKh6hGpiU\nsnNMX5cs9ClSNao+K03kx3YBnfbVZtPe1tbc8p6A7EqN/W/US37nCl+Tjak8eUOw\nDL9wGM82hpYoTXaEiKgsYGM0WqObIE6R5AN1ukW5wyUZYef6MIwc94LLQQKBgQCQ\nheUlNbtOtzmq+rXF8wIk424L8fuwj4Onk3pcII4zQD4eHW20FIQ9aq2sFiaLwc3g\nM+ELdn7w55HweD+gErD1pcxE0qdiENWkFOJCNMNkw5l7Z8ju0o7gpT0p6GBgNz5Z\nkI2pvf5AeuLz3CmwP8Q2K9MVaC63GXkZie5mNl8qxwKBgDOhNr5xQs8OxV7dGAZb\nOvXuMqJ1+yeceXyBtaa5JZD+y+H5mS1cPvIYvpyHQLqQQjatFw2EKXuOtC5ENUJa\ntOaGdzyp9LaKiZgumCP1Jh9C+2N7fL8SN7oY9h/QNyc1aNdc0FWvASZ+oIHCJDoO\nffbNKE0pcEq/Z3yP94avJYqc\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-cipze@makersgully.iam.gserviceaccount.com",
+  "client_id": "114008192538960257854",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-cipze%40makersgully.iam.gserviceaccount.com"
+}
